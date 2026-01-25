eb8616c8a64d86503e03a9f47e9be7e7ff54a035
DenisVenev <denisvenev997@gmail.com>
2026-01-26 01:57:42 +0200

    Added one more project :)
    Just a program from the book's tasks

---
+++ C# course/ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string name;
+
+            Console.Title = "Let's meet...";
+            Console.WriteLine("What's your name?");
+            name = Console.ReadLine();
+
+            string txt = $"Nice to meet you {name}!";
+            Console.Title = "We are friends!";
+            Console.WriteLine(txt);
+
+            Console.ReadKey();
+
+        }
+    }
+}
