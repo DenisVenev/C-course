24bde6d73cd4f4943f95961f447777a54c0f7936
DenisVenev <denisvenev997@gmail.com>
2026-02-05 22:00:27 +0200

    Fibbonachi numbers
    This is one of the excersices I solve in the book for C# :)

---
+++ Project2/CodeFile1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+
+class Tasks
+{
+
+    static void Main()
+    {
+
+		int n;
+		int curr = 1;
+		int prev = 1;
+
+		try
+		{
+
+			n = Int32.Parse(Interaction.InputBox("How much numbers do you want to calculate?", "Fibbonachi"));
+			for (int i = 0; i < n; i++) {
+
+				MessageBox.Show($"{curr}");
+				prev = curr - prev;
+				curr = prev + curr;
+				
+			}
+			
+
+		}
+		catch
+		{
+			MessageBox.Show("Maika ti", "Fail", MessageBoxButtons.OK, MessageBoxIcon.Error);
+		}
+        
+    }
+
+}
